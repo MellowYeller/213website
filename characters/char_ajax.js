037b33e39daad9e4e9c45a69132c68ec579bb89b
mrstarch <mrstarch@me.com>
2020-11-27 16:16:00 -0800

    working select

--- characters/char_ajax.js
+++ characters/char_ajax.js
@@ -3,7 +3,7 @@ function createChar() {
     const str = document.getElementById('strength').value;
     const agi = document.getElementById('agility').value;
     const inte = document.getElementById('intelligence').value;
-    
+
     if (name === "") {
         document.getElementById("createresponse").innerHTML = "break";
         return;
@@ -12,11 +12,30 @@ function createChar() {
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
                 document.getElementById("createresponse").innerHTML = this.responseText;
+                getChar();
             }
         };
         xhttp.open("POST", "createChar.php", true);
         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhttp.send("name=" + name + "&str=" + str + "&agi=" + agi + "&inte=" + inte);
+    }
+}
+function getChar() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("exisChar").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getChar.php", true);
+    xhttp.send();
+}
+
+function checkChar() {
+    const x = document.forms["select"]["exisChar"].value;
+    if (x === '') {
+        alert("Please Create a Character First");
+        return false;
     }
 }
 
